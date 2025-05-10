7a67d9c2e75dd95f681f34e7fd4615b6698e27ec
devneeraj77 <dev.neerajrekwar@gmail.com>
2025-05-10 11:36:26 +0000

    f

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
         <Link
           isExternal
           className={buttonStyles({ variant: "bordered", radius: "full" })}
-          href={siteConfig.links.github}
+          href={siteConfig.links.instagram}
         >
           <GithubIcon size={20} />
           GitHub
